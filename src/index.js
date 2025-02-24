5cc61627b6e848098caa82bac2b7acc4afe22c56
Merey-git <merei.kuanyshkhan@narxoz.kz>
2025-02-24 23:36:20 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Импортируем createRoot вместо render
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Создаем корневой элемент с помощью createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Используем root.render для рендеринга компонента
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// Вы можете оставить функцию reportWebVitals, если хотите отслеживать производительность
 reportWebVitals();
